0f05e477f65e713e7377973aac7ef38df7eef4cc
Yuliia Shaposhnikova <domik339@gmail.com>
2025-05-23 20:57:31 +0300

    added new solutions for 5 kyu

--- codewars/YuliaShap/5kyu.py
+++ codewars/YuliaShap/5kyu.py
@@ -1,15 +1,44 @@
 # Gap in Primes
 
 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def gap(g, m, n):
+    last_prime = None
+    for num in range(m, n + 1):
+        if is_prime(num):
+            if last_prime is not None and num - last_prime == g:
+                return [last_prime, num]
+            last_prime = num
+    return None
 
 # Trailing zeros in factorial
 
 
-
+def zeros(n):
+    count = 0
+    while n >= 5:
+        n = n // 5
+        count += n
+    return count
 
 # Perimeter of squares in a rectangle
 
 
+def perimeter(n):
+    a, b = 1, 1
+    total = a
+    for _ in range(n):
+        a, b = b, a + b
+        total += a
+    return 4 * total
 
 # Which x for that sum?
 
