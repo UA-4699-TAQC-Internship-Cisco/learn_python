c9389039f35a853d64248453b1ab7f8d6ea68fee
Yuliia Shaposhnikova <domik339@gmail.com>
2025-05-27 11:03:39 +0300

    added new solutions for 5 kyu

--- codewars/YuliaShap/5kyu.py
+++ codewars/YuliaShap/5kyu.py
@@ -43,8 +43,40 @@ def perimeter(n):
 # Which x for that sum?
 
 
+def solve(m):
+    left = 0.0
+    right = 1.0
+
+    while right - left > 1e-13:
+        mid = (left + right) / 2
+        f = mid / (1 - mid) ** 2
+
+        if f < m:
+            left = mid
+        else:
+            right = mid
+
+    return (left + right) / 2
 
 # Find the smallest
+
+
+def smallest(n):
+    s = str(n)
+    min_triplet = [n, 0, 0]
+
+    for i in range(len(s)):
+        digit = s[i]
+        rest = s[:i] + s[i+1:]
+        for j in range(len(s)):
+            new_s = rest[:j] + digit + rest[j:]
+            new_n = int(new_s)
+            if new_n < min_triplet[0] or \
+               (new_n == min_triplet[0] and i < min_triplet[1]) or \
+               (new_n == min_triplet[0] and i == min_triplet[1] and j < min_triplet[2]):
+                min_triplet = [new_n, i, j]
+
+    return min_triplet
 
 
 
